eb14cbf1c1389eabb0d8ee981451d257ddaf0bc9
John Morris <70734743+jmorris335@users.noreply.github.com>
2024-10-07 22:31:18 -0400

    Added basic relations.

---
+++ src/constrainthg/relations.py
@@ -0,0 +1,75 @@
+"""
+File: relations.py
+Author: John Morris, jhmrrs@clemson.edu, https://orcid.org/0009-0005-6571-1959
+Purpose: A list of basic relations employable with edges in the hypergraph.
+License: All rights reserved.
+Versions:
+- 0.0, 7 Oct. 2024: initialized
+"""
+
+import numpy as np
+
+# AUX FUNCTIONS
+def extend(args: list, kwargs: dict)-> list:
+    """Combines all arguments into a single list, with args leading."""
+    return list(args) + list(kwargs.values())
+
+def extendWithExceptions(args: list, kwargs: dict, excluded_keys: list):
+    """Combines all arguments except those with a given key."""
+    if not isinstance(excluded_keys, list):
+        excluded_keys = [excluded_keys]
+    args = list(args) + [kwargs[key] for key in kwargs if key in excluded_keys]
+    return args
+
+# ALGEBRAIC RELATIONS
+def Rsum(*args, **kwargs):
+    """Sums all arguments."""
+    args = extend(args, kwargs)
+    return sum(args)
+
+def Rmuliply(*args, **kwargs):
+    """Multiplies all arguments together."""
+    args = extend(args, kwargs)
+    out = 1
+    for s in args:
+        out *= s
+    return out
+
+def Rsubtract(*args, **kwargs):
+    """Subtracts from `s1` all other arguments."""
+    args = extendWithExceptions(args, kwargs, 's1')
+    if 's1' in kwargs:
+        return kwargs['s1'] - sum(args)
+    else:
+        return args[0] - sum(args[1:])
+    
+def Rdivide(*args, **kwargs):
+    """Divides `s1` by all other arguments."""
+    args = extendWithExceptions(args, kwargs, 's1')
+    if 's1' in kwargs:
+        s1 = kwargs['s1']
+    else:
+        s1 = args[0]
+        args = args[1:]
+    for s in args:
+        s1 /= s
+    return s1
+
+def Rnegate(*args, **kwargs):
+    """Returns the negative of the first argument."""
+    args = extend(args, kwargs)
+    return -args[0]
+
+def Rinvert(*args, **kwargs):
+    """Inverts the first argument."""
+    args = extend(args, kwargs)
+    return 1 / args[0]
+
+def Rmean(*args, **kwargs):
+    """Returns the mean of all arguments."""
+    args = extend(args, kwargs)
+    return np.mean(args)
+
+if __name__ == '__main__':
+    a = Rsubtract(4, s1=10)
+    print(a)
---
+++ test/test_hypergraph.py
@@ -0,0 +1,28 @@
+from src.constrainthg.hypergraph import Hypergraph
+import src.constrainthg.relations as R
+
+class TestSimple():
+    def test_simple_add(self):
+        hg = Hypergraph()
+        hg.addEdge(['A', 'B'], 'C', R.Rsum)
+        inputs = {'A': 100, 'B': 12.9}
+        assert hg.DFS(inputs, 'C') == 112.9, "Sum should be 112.9"
+
+    def test_simple_multiply(self):
+        hg = Hypergraph()
+        hg.addEdge(['A', 'B'], 'C', R.Rmuliply)
+        inputs = {'A': 3, 'B': 1.5}
+        assert hg.DFS(inputs, 'C') == 4.5, "Product should be 4.5"
+
+    def test_simple_subtract(self):
+        hg = Hypergraph()
+        hg.addEdge(['A', 'B'], 'C', R.Rsubtract)
+        inputs = {'A': 3, 'B': 2}
+        assert hg.DFS(inputs, 'C') == 1, "Product should be 4.5"
+
+    def test_simple_void(self):
+        hg = Hypergraph()
+        via_le10 = lambda *source : all([s < 10 for s in source])
+        hg.addEdge(['A', 'B'], 'C', R.Rsum, via=via_le10)
+        inputs = {'A': 100, 'B': 51}
+        assert hg.DFS(inputs, 'C') == None, "Should have invalid condition"
