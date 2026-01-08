f7327da7c5760955c3a6a91bf3e6ab00a512bae0
John Morris <70734743+jmorris335@users.noreply.github.com>
2026-01-08 16:45:08 -0500

    release 0.3.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ project = 'ConstraintHg'
 copyright = '2024, John Morris'
 license = 'Apache 2.0'
 author = 'John Morris'
-release = '0.3.0'
+release = '0.3.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
